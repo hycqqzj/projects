360bd8c9ddb2c2fc513cdf61434c0f0927ac9716
hycqqzj <735376819@qq.com>
2019-06-25 22:06:48 +0800

    log

--- hello-web/src/main/java/com/hyc/controller/HelloController.java
+++ hello-web/src/main/java/com/hyc/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.hyc.controller;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sun.plugin2.message.JavaScriptBaseMessage;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
 @Controller
+@Slf4j
 public class HelloController {
     @Value("${application.message:Hello World}")
     private String message = "Hello World";
@@ -16,11 +21,13 @@ public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello(){
+        log.info("执行hello请求，时间：{}", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         return "hello";
     }
 
     @GetMapping("/welcome")
     public String welcome(Map<String, Object> model) {
+        log.info("执行welcome请求，时间：{}", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         model.put("time", new Date());
         model.put("message", this.message);
         return "welcome";
