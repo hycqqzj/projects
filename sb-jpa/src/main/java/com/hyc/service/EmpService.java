27e755d9a0ce8d041df396750d5fee6b54424de2
hycqqzj <735376819@qq.com>
2019-07-16 10:38:36 +0800

    JPA

---
+++ sb-jpa/src/main/java/com/hyc/service/EmpService.java
@@ -0,0 +1,28 @@
+package com.hyc.service;
+
+import com.hyc.dao.EmpDao;
+import com.hyc.entity.Emp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmpService {
+    @Autowired
+    private EmpDao empDao;
+
+    public Emp findByName(String name){
+        return empDao.findByName(name);
+    }
+
+    public Emp findByCode(String code){
+        return empDao.findByCode(code);
+    }
+
+    public Emp insert(Emp emp){
+        return empDao.save(emp);
+    }
+
+    public void deleteById(Long id) {
+        empDao.deleteById(id);
+    }
+}
