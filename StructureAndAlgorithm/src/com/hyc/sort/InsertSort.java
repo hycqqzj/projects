3c4782d298986252e9d9fb52c9faece7fc455690
hycqqzj <735376819@qq.com>
2019-07-19 11:36:56 +0800

    排序算法

---
+++ StructureAndAlgorithm/src/com/hyc/sort/InsertSort.java
@@ -0,0 +1,36 @@
+package com.hyc.sort;
+
+import com.hyc.util.SwapUtil;
+
+public class InsertSort {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 16, 15, 17, 8, 10, 9, 25, 21, 24, 22, 28 };
+		sort(arr);
+		for (int i = 0; i <= arr.length - 1; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	/**
+	 * 插入排序：
+	 * 在要排序的一组数中，假定前n-1个数已经排好序，现在将第n个数插到前面的有序数列中，使得这n个数也是排好顺序的
+	 * 如此反复循环，直到全部排好顺序
+	 * 
+	 * @param arr
+	 */
+	private static void sort(int[] arr) {
+		for (int i = 1; i <= arr.length - 1; i++) {
+			for (int j = i; j > 0; j--) {
+				if (arr[j] < arr[j - 1]) {
+					SwapUtil.swap(arr, j - 1, j);
+				} else {
+					// 已经是有序的不需要遍历
+					break;
+				}
+			}
+		}
+
+	}
+
+}
---
+++ StructureAndAlgorithm/src/com/hyc/util/SwapUtil.java
@@ -0,0 +1,18 @@
+package com.hyc.util;
+
+public class SwapUtil {
+	
+	/**
+	 * 交换数组中第i个元素和第j个元素
+	 * 
+	 * @param arr
+	 * @param i
+	 * @param j
+	 */
+	public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+}
