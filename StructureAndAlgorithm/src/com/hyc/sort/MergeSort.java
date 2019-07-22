7bf1595d00077c9ff37080e075f7bfe0d1379f14
hycqqzj <735376819@qq.com>
2019-07-22 18:09:33 +0800

    排序算法

---
+++ StructureAndAlgorithm/src/com/hyc/sort/MergeSort.java
@@ -0,0 +1,61 @@
+package com.hyc.sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 15, 17, 8, 10, 9, 25, 21, 24, 22, 28 };
+
+		sort(arr, 0, arr.length - 1);
+		for (int i = 0; i <= arr.length - 1; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	/**
+	 * 归并排序：
+	 * 将数组分成2组A，B，如果这2组组内的数据都是有序的，那么就可以很方便的将这2组数据进行排序
+	 * 可以将A，B组各自再分成2组。依次类推，当分出来的小组只有1个数据时，可以认为这个小组组内已经达到了有序，然后再合并相邻的2个小组就可以了
+	 * 平均时间复杂度：O(N*logN)
+	 * 
+	 * @param arr
+	 * @param begin
+	 * @param end
+	 */
+	private static void sort(int[] arr, int begin, int end) {
+		int mid = (begin + end) / 2;
+
+		if (begin < end) {
+			sort(arr, begin, mid);
+			sort(arr, mid + 1, end);
+			mergeArr(arr, begin, end);
+		}
+	}
+
+	private static void mergeArr(int[] arr, int begin, int end) {
+		int middle = (begin + end) / 2;
+
+		int i = 0;
+		int j = 0;
+		int k = begin;
+
+		int[] left = Arrays.copyOfRange(arr, begin, middle + 1);
+		int[] right = Arrays.copyOfRange(arr, middle + 1, end + 1);
+		while (i < left.length && j < right.length) {
+			if (left[i] < right[j]) {
+				arr[k++] = left[i++];
+			} else {
+				arr[k++] = right[j++];
+			}
+		}
+
+		while (i < left.length) {
+			arr[k++] = left[i++];
+		}
+		while (j < right.length) {
+			arr[k++] = right[j++];
+		}
+	}
+
+}
---
+++ StructureAndAlgorithm/src/com/hyc/sort/QuickSort.java
@@ -0,0 +1,48 @@
+package com.hyc.sort;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 15, 17, 8, 10, 9, 25, 21, 24, 6 };
+		sort(arr, 0, arr.length - 1);
+		for (int i = 0; i <= arr.length - 1; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	/**
+	 * 快速排序
+	 * 平均时间复杂度：O(N*logN)
+	 * @param arr
+	 * @param low
+	 * @param high
+	 */
+	private static void sort(int[] arr, int low, int high) {
+		if (low >= high) {
+            return;
+        }
+		
+		int i = low;
+		int j = high;
+		int index = arr[i];
+		while (i < j) {
+			while (i < j && arr[j] >= index) {
+				j--;
+			}
+			if(i < j) {
+				arr[i++] = arr[j];
+			}
+			
+			while(i < j && arr[i] <= index) {
+				i++;
+			}
+			if(i < j) {
+				arr[j--] = arr[i];
+			}
+		}
+		arr[i] = index; // 将基准数填入最后的坑
+		sort(arr, low, i - 1); // 递归调用，分治
+		sort(arr, i + 1, high); // 递归调用，分治
+	}
+
+}
