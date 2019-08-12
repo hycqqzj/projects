84105d510591ee897e1254d8a5ca3b8d6c81a88d
hycqqzj <735376819@qq.com>
2019-08-12 12:29:50 +0800

    动态规划

---
+++ StructureAndAlgorithm/src/com/hyc/algorithm/dynamicplan/MaxSeq.java
@@ -0,0 +1,61 @@
+package com.hyc.algorithm.dynamicplan;
+
+import lombok.Data;
+
+/**
+ * 整型数组的最大增长子序列
+ */
+@Data
+public class MaxSeq {
+	private int[] data;
+	private int[] seq;
+
+	public MaxSeq(int[] data) {
+		this.data = data;
+		this.seq = new int[data.length];
+		this.seq[0] = 1;
+	}
+
+	public int getMaxSeq() {
+		for (int i = 1; i < data.length; i++) {
+			int preMax = maxSeqFirstLessOrEq(data[i], i);
+			seq[i] = preMax + 1;
+		}
+
+		int max = 0;
+		for (int j = 0; j < seq.length; j++) {
+			if (seq[j] > max)
+				max = seq[j];
+		}
+
+		return max;
+	}
+
+	private int maxSeqFirstLessOrEq(int a, int aLoc) {
+		int loc = -1;
+		int[] temp = new int[aLoc];
+		for (int i = aLoc - 1; i >= 0; i--) {
+			if (data[i] <= a) {
+				temp[++loc] = seq[i];
+			}
+		}
+
+		if (loc < 0) {
+			return 0;
+		}
+
+		int max = temp[0];
+		for (int i = 1; i < temp.length; i++) {
+			if (temp[i] > max)
+				max = temp[i];
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 9, 3, 6, 5, 1, 7 };
+		MaxSeq maxSeq = new MaxSeq(arr);
+		System.out.println(maxSeq.getMaxSeq());
+	}
+
+}
---
+++ StructureAndAlgorithm/src/com/hyc/algorithm/dynamicplan/YHTriangle.java
@@ -0,0 +1,73 @@
+package com.hyc.algorithm.dynamicplan;
+
+import lombok.Data;
+
+/**
+ * 杨辉三角
+ */
+@Data
+public class YHTriangle {
+	private int[][] data;
+
+	public YHTriangle(int[][] data) {
+		this.data = data;
+	}
+
+	/**
+	 * 计算最短路径（递归方法：第i,j个位置最小路径值为其左边最小路径和右边最小路径中的最小值+当前节点的值）
+	 * 动态规划方法-状态转移方程法
+	 * 
+	 * @param line行
+	 * @param column列
+	 * @return
+	 */
+	public int minPathDG(int line, int column) {
+		if (line == data.length - 1) {
+			return data[line][column];
+		}
+
+		return data[line][column] + Math.min(minPathDG(line + 1, column), minPathDG(line + 1, column + 1));
+	}
+
+	/**
+	 * 计算最短路径（动态规划方法-状态转移表法：第i,j个位置最小路径值为data[i - 1][j]和data[i - 1][j - 1]中的最小值+当前节点的值）
+	 * 
+	 * @return
+	 */
+	public int minPathPlan() {
+		for (int i = 1; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if (j == 0) {
+					data[i][0] = data[i][0] + data[i - 1][0];
+				} else {
+					if (data[i - 1][j] > 0 && data[i - 1][j - 1] > 0 && data[i][j] > 0) {
+						data[i][j] = Math.min(data[i - 1][j], data[i - 1][j - 1]) + data[i][j];
+					} else if (data[i][j] > 0) {
+						data[i][j] = data[i - 1][j - 1] + data[i][j];
+					}
+				}
+			}
+		}
+
+		int min = data[data.length - 1][0];
+		for (int m = 0; m < data[data.length - 1].length; m++) {
+			if (data[data.length - 1][m] > 0 && data[data.length - 1][m] < min) {
+				min = data[data.length - 1][m];
+			}
+		}
+		return min;
+	}
+
+	public static void main(String[] args) {
+		int[][] data1 = new int[][] { { 5, -1, -1, -1, -1 }, { 7, 8, -1, -1, -1 }, { 2, 3, 4, -1, -1 },
+				{ 4, 9, 6, 1, -1 }, { 2, 7, 9, 4, 5 } };
+		YHTriangle yh1 = new YHTriangle(data1);
+		System.out.println(yh1.minPathDG(0, 0));
+
+		int[][] data2 = new int[][] { { 5, -1, -1, -1, -1 }, { 7, 8, -1, -1, -1 }, { 2, 3, 4, -1, -1 },
+				{ 4, 9, 6, 1, -1 }, { 2, 7, 9, 4, 5 } };
+		YHTriangle yh2 = new YHTriangle(data2);
+		System.out.println(yh2.minPathPlan());
+	}
+
+}
