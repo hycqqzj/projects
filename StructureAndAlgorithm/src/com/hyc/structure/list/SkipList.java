242c9f422ad76f5bf923b9a48d705ee4366c8ac8
hycqqzj <735376819@qq.com>
2019-09-25 11:39:44 +0800

    skip

--- StructureAndAlgorithm/src/com/hyc/structure/list/SkipList.java
+++ StructureAndAlgorithm/src/com/hyc/structure/list/SkipList.java
@@ -4,14 +4,29 @@ import java.util.Random;
 
 /**
  * 跳表的一种实现方法。 跳表中存储的是正整数，并且存储的是不重复的。
+ * 
+#1. 结构
+  5 head->2
+  4 head->1->2
+  3 head->1->2->4->5->6
+  2 head->1->2->3->4->5->6
+  1 head->1->2->3->4->5->6
+  0 head->1->2->3->4->5->6
+Node中的forward[]表示为 第i层 该Node节点的下一个节点 如 head的forward[0] 代表为第0层下一个节点 即1
+
+#2. 插入函数理解
+（1）生成随机层数函数 在这层以及下层都要插入这个值
+（2）update[i] 函数表示第 i 层要更新的节点 ，每层都初始化指向head节点。
+（3）找出每一层最后一个小于value的节点 记录最后一个小于value的节点。
+（4）然后将value插入到这个节点后面。更新每层节点
  */
 public class SkipList {
-	private static final int MAX_LEVEL = 16;
-
+	// 索引层和底层相加最大数
+	private static final int MAX_LEVEL = 5;
+	
 	private int levelCount = 1;
-
-	private Node head = new Node(); // 带头链表
-
+	// 带头链表
+	private Node head = new Node();
 	private Random random = new Random();
 
 	public Node find(int value) {
@@ -31,9 +46,11 @@ public class SkipList {
 
 	public void insert(int value) {
 		int level = randomLevel();
+		
 		Node newNode = new Node();
 		newNode.data = value;
 		newNode.maxLevel = level;
+		
 		Node update[] = new Node[level];
 		for (int i = 0; i < level; ++i) {
 			update[i] = head;
@@ -42,6 +59,7 @@ public class SkipList {
 		// record every level largest value which smaller than insert value in update[]
 		Node p = head;
 		for (int i = level - 1; i >= 0; --i) {
+			// 找到新节点插入的位置，即找到小于新值的最后一个节点
 			while (p.forwards[i] != null && p.forwards[i].data < value) {
 				p = p.forwards[i];
 			}
@@ -50,13 +68,17 @@ public class SkipList {
 
 		// in search path node next node become new node forwords(next)
 		for (int i = 0; i < level; ++i) {
+			// 通过指针操作加入新的节点，类似于单链表的插入操作
+			// 新节点的next指针指向前一个节点的next指针
 			newNode.forwards[i] = update[i].forwards[i];
+			// 前一个节点的next指针指向新节点
 			update[i].forwards[i] = newNode;
 		}
 
-		// update node hight
-		if (levelCount < level)
+		// 更新调表的深度
+		if (levelCount < level) {
 			levelCount = level;
+		}
 	}
 
 	public void delete(int value) {
@@ -78,7 +100,11 @@ public class SkipList {
 		}
 	}
 
-	// 随机 level 次，如果是奇数层数 +1，防止伪随机
+	/**
+	 * 随机 level 次，如果是奇数层数 +1，防止伪随机
+	 * 
+	 * @return
+	 */
 	private int randomLevel() {
 		int level = 1;
 		for (int i = 1; i < MAX_LEVEL; ++i) {
@@ -101,6 +127,7 @@ public class SkipList {
 
 	public class Node {
 		private int data = -1;
+		// forward[]表示为 第i层中该Node节点的下一个节点，如 head的forward[0]代表为第0层下一个节点
 		private Node[] forwards = new Node[MAX_LEVEL];
 		private int maxLevel = 0;
 
@@ -119,9 +146,12 @@ public class SkipList {
 
 	public static void main(String[] args) {
 		SkipList list = new SkipList();
-		for (int i = 1; i <= 50000; i++) {
+		for (int i = 1; i <= 10; i++) {
 			list.insert(i);
 		}
-		System.out.println(25369);
+		
+		list.printAll();
+		
+		System.out.println(list.find(6));
 	}
 }
